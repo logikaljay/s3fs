52150f84b5c41409c178fa9a11674631406c40fb
Jay Baker <logikal@gmail.com>
2022-04-25 12:37:21 +1200

    chore: add typedoc example to unlink

--- src/unlink.ts
+++ src/unlink.ts
@@ -2,6 +2,15 @@ import { getClient } from "./client.js"
 import { DeleteObjectCommand } from "@aws-sdk/client-s3"
 import { destructurePath } from "./utils/destructurePath.js"
 
+/**
+ * @example
+ * ```typescript
+ * import { getClient } from "@logikaljay/s3fs"
+ * 
+ * const client = await getClient()
+ * await client.unlink('my-bucket/screenshot-to-delete.png')
+ * ```
+ */
 export async function unlink(remotePath: string) {
   let client = await getClient()
   let { Bucket, Key } = destructurePath(remotePath)
